5c87f3f052c803f8ec91cd2b22f9723d69795be8
mjuyo <150392024+mjuyo@users.noreply.github.com>
2024-03-16 23:27:19 -0500

    Added connect.php and updated index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+/*******w******** 
+    
+    Name: Marco Juyo
+    Date: March 16th, 2024
+    Description: PHP Home page for project
+
+****************/
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+</head>
+<body>
+
+</body>
+</html>
