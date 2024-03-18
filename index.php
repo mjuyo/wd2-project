b25de7f73b2a033c518327d0f7c956e67158335f
mjuyo <150392024+mjuyo@users.noreply.github.com>
2024-03-17 23:47:17 -0500

    Added the connection to the table and markup to update the rows.

--- index.php
+++ index.php
@@ -8,7 +8,19 @@
 
 ****************/
 
+    require('connect.php');
 
+    $query = "SELECT * FROM bounties ORDER BY bounty_date DESC";
+
+    $statement = $db->prepare($query);
+
+    $statement->execute();
+
+    function is_active($link) {
+        // Get the current page file name
+        $current_page = basename($_SERVER['PHP_SELF']); 
+        return $link == $current_page ? 'active' : '';
+    }
 ?>
 
 <!DOCTYPE html>
@@ -19,6 +31,29 @@
     <title></title>
 </head>
 <body>
+    <div id="wrapper">
+        <div id="header">
+            <h1><a href="index.php">Galactic Bounties Network - GBN</a></h1>
+        </div>
+        <?php include('nav.php'); ?>
+        <div id="all-blogs">
+            <div class="blog-post">
+                <?php while($row = $statement->fetch()): ?>
+                <div class="paragraph">
+                    <h2><a href="show.php?id=<?= $row['id'] ?>"><?= $row['title'] ?></a></h2>
+                    <div class="date-stamp">
+                        <small><?= date("F d, Y, h:i a", strtotime($row['bounty_date'])) . " - "?><a href="edit.php?id=<?= $row['id'] ?>">edit</a></small>
+                    </div>
+                    <div class="blog-content">
+                        <p><?= $row['description'] ?></p>
+                    </div>
+                </div>
+                <?php endwhile ?>
+            </div>
+        </div>
+        <!-- <?php include('footer.php'); ?> -->
+    </div>
+
 
 </body>
 </html>
