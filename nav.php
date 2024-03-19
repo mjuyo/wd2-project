27290ddc1136a79f640d2c39ad7787ca3f18c935
mjuyo <150392024+mjuyo@users.noreply.github.com>
2024-03-19 01:02:18 -0500

    Added and updated nav.php

--- nav.php
+++ nav.php
@@ -1,6 +1,6 @@
 <div id="navbar">
     <ul id="menu">
         <li><a href="index.php" class="<?= is_active('index.php') ?>">Home</a></li>
-        <li><a href="post.php" class="<?= is_active('post.php') ?>">New Bounty</a></li>
+        <li><a href="bounty.php" class="<?= is_active('bounty.php') ?>">New Bounty</a></li>
     </ul>
 </div>
