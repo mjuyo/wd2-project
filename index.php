b89d9af6e49139863dff876178bb7bece88ff3d9
mjuyo <150392024+mjuyo@users.noreply.github.com>
2024-03-25 23:19:43 -0500

    index.php structure updated, styles.css updated with new structure

--- index.php
+++ index.php
@@ -31,41 +31,42 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;500&display=swap" rel="stylesheet">
+    <link href="https://fonts.cdnfonts.com/css/aurebesh" rel="stylesheet">
+                
     <link rel="stylesheet" href="styles.css">
     <title>GBN</title>
 </head>
 <body>
     <div id="wrapper">
-        <div id="header">
+        <header id="header">
             <h1><a href="index.php">Galactic Bounties Network</a></h1>
-        </div>
+        </header>
         <?php include('nav.php'); ?>
-        <div id="all-bounties">
-            <div class="bounties-post">
+        <main id="all-bounties">
+            <section class="bounties-grid">
                 <?php while($row = $statement->fetch()): ?>
-                <div class="paragraph">
-                    <h2><a href="details.php?bounty_id=<?= $row['bounty_id'] ?>"><?= $row['title'] ?></a></h2>
-                    <div class="date-stamp">
-                        <small><?= date("F d, Y, h:i a", strtotime($row['bounty_date'])) . " - "?><a href="edit.php?bounty_id=<?= $row['bounty_id'] ?>">edit</a></small>
+                    <div class="bounties-post">
+                        <h2><a href="details.php?bounty_id=<?= $row['bounty_id'] ?>"><?= $row['title'] ?></a></h2>
+                        <div class="date-stamp">
+                            <small><?= date("F d, Y, h:i a", strtotime($row['bounty_date'])) . " - "?><a href="edit.php?bounty_id=<?= $row['bounty_id'] ?>">edit</a></small>
+                        </div>
+                        <div class="bounties-content">
+                            <?php if (!empty($row['image_path'])): ?>
+                                <img src="<?= $row['image_path'] ?>" alt="<?= $row['title'] ?>">
+                            <?php elseif (empty($row['image_path'])): ?>
+                                <img src="images/hutt_logo.jpg" alt="No Image">
+                            <?php endif ?>
+                            <p><strong>Description:</strong> <?= $row['description'] ?></p>
+                            <p><strong>Name:</strong> <?= $row['name'] ?></p>
+                            <p><strong>Species:</strong> <?= $row['species'] ?></p>
+                            <p><strong>Reward:</strong> <?= $row['reward'] ?> <span class="special-font">$ </span></p>
+                            <p><strong>Status:</strong> <?= $row['status'] ?></p>
+                        </div>
                     </div>
-                    <div class="bounties-content">
-                        <?php if (!empty($row['image_path'])): ?>
-                            <img src="<?= htmlspecialchars($row['image_path']) ?>" alt="<?= htmlspecialchars($row['title']) ?>">
-                        <?php endif ?>
-                        <p><strong>Description:</strong> <?= htmlspecialchars($row['description']) ?></p>
-                        <p><strong>Name:</strong> <?= htmlspecialchars($row['name']) ?></p>
-                        <p><strong>Species:</strong> <?= htmlspecialchars($row['species']) ?></p>
-                        <p><strong>Reward:</strong> <?= htmlspecialchars($row['reward']) ?></p>
-                        <p><strong>Status:</strong> <?= htmlspecialchars($row['status']) ?></p>
-
-                    </div>
-                </div>
                 <?php endwhile ?>
-            </div>
-        </div>
+            </section>
+        </main>
         <?php include('footer.php'); ?>
     </div>
-
-
 </body>
 </html>
