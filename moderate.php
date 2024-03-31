f02d9d4c6fc1b4bddd3f5f1effe941aae3946fec
mjuyo <150392024+mjuyo@users.noreply.github.com>
2024-03-31 15:27:57 -0500

    Creatd a moderate.php with script to handle the deletion of a comment by admin users

---
+++ moderate.php
@@ -0,0 +1,23 @@
+<?php
+    session_start();
+    require('connect.php');
+
+    if (!isset($_SESSION['is_admin']) || $_SESSION['is_admin'] !== true) {
+        header('Location: signin.php');
+        exit;
+    }
+
+    if (isset($_GET['comment_id'])) {
+        $comment_id = filter_input(INPUT_GET, 'comment_id', FILTER_SANITIZE_NUMBER_INT);
+
+        $query = "DELETE FROM comments WHERE comment_id = :comment_id";
+        $statement = $db->prepare($query);
+        $statement->bindValue(':comment_id', $comment_id, PDO::PARAM_INT);
+        
+        if ($statement->execute()) {
+            // Redirect back to the comments page
+            header("Location: details.php?bounty_id=" . $bountyId); 
+            exit;
+        }        
+    }
+?>
