84318317a1ff405f1c20dbc31f0f47cf1a979cb8
mjuyo <150392024+mjuyo@users.noreply.github.com>
2024-03-31 15:30:14 -0500

    The file nav.php was replaced by header.php

--- nav.php
+++
@@ -1,6 +0,0 @@
-<div id="navbar">
-    <ul id="menu">
-        <li><a href="index.php" class="<?= is_active('index.php') ?>">Home</a></li>
-        <li><a href="bounty.php" class="<?= is_active('bounty.php') ?>">New Bounty</a></li>
-    </ul>
-</div>
